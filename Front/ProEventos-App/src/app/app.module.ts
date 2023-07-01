fc02a907fe364767103379a457ef13ad091a8389
ArleiRodrigues1 <Arlei.freire011@gmail.com>
2023-07-01 11:21:12 -0300

    Agular: rotas e alertas

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,33 +1,61 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { TitleComponent } from './shared/title/title.component';
+
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+import { EventoService } from './services/Evento.service';
+import { timeout } from 'rxjs';
+
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
-      NavComponent
+    PerfilComponent,
+    DashboardComponent,
+    ContatosComponent,
+    TitleComponent,
+    DateTimeFormatPipe
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
-    FormsModule
+    CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-left',
+      preventDuplicates: true,
+      progressBar: true
+    }),
   ],
-  providers: [],
+  providers: [
+    EventoService
+  ],
   bootstrap: [AppComponent],
 })
 
 export class AppModule { }
+
---
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,81 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+
+import { Evento } from '../../models/Evento';
+import { EventoService } from '../../services/Evento.service';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+
+export class EventosComponent  implements OnInit {
+
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+
+  public mostrarImg: boolean = true;
+  private _filtroLista: string = '';
+
+  modalRef = {} as BsModalRef;
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista): this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastrService: ToastrService
+    ) {}
+
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  public confirm(): void {
+    this.modalRef.hide();
+    this.toastrService.success("O evento foi deletado com sucesso")
+  }
+
+  public decline(): void {
+    this.modalRef.hide();
+  }
+
+  ngOnInit(): void{
+    this.getEventos();
+  }
+
+  public alterarImg() : void{
+    this.mostrarImg = !this.mostrarImg;
+
+  }
+
+ public getEventos(): void  {
+  this.eventoService.getEventos().subscribe({
+    next: (eventos: Evento[]) => {
+      this.eventos = eventos;
+      this.eventosFiltrados = this.eventos;
+    },
+    error: (error: any) => console.log(error)
+  });
+ }
+}
